9577a1708861b793c2047aa43b60a4d020d484b8
lucabozzetti <luca.bozzetti8@gmail.com>
2025-05-18 18:18:30 +0200

    test

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import os
 
 from dotenv import load_dotenv
 from openai import OpenAI
-from adk_test.visualizer_test.system_prompt import vis_prompt
+from system_prompt import vis_prompt
 from pydantic import BaseModel
 
-### CONFIGURE ENVIRONMENT
 
+# CONFIGURE ENVIRONMENT
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
-### DEFINE OUTPUT SCHEMA (Add Form(description) here) TODO
 
+# DEFINE OUTPUT SCHEMA (Add Form(description) here) TODO
 class Visualization(BaseModel):
     caption: str
     code: str
     description: str
 
+
+# PROMPT OPENAI
 response = client.responses.parse(
-            model="gpt-4o",
+            model="gpt-4o-mini",
             text_format=Visualization,
             input=[
                 {"role": "system", "content": vis_prompt},
